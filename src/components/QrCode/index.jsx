97e67bc77e703895d9b3f64ca3411bae8ca0fda2
vargas991 <edderson.vargas@unet.edu.ve>
2022-07-19 02:26:50 -0500

    solve error to image

--- src/components/QrCode/index.jsx
+++ src/components/QrCode/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './styles.css'
-
+import qr from '../../../public/images/image-qr-code.png'
 export const QrCode = () => {
   return (
     <main class="main-container">
     <section class="card">
       <article class="card-img">
-        <img src={'../../../public/images/image-qr-code.png'} alt="" />
+        <img src={'../images/image-qr-code.png'} alt="" />
       </article>
       <article class="card-info">
         <h1 class="card-info__title" >Improve your front-end skills by building projects</h1>
